757044791b3046a4d64fbebe1538de812925c6a7
mpleier <mpleier@gmail.com>
2017-07-24 14:03:01 -0700

    added

---
+++ assets/javascript/game.js
@@ -0,0 +1,64 @@
+function myScript(){
+    console.log("working");
+}
+
+
+
+var chars = [
+  "a","b","c","d","e","f","g","h","i","j",
+  "k","l","m","n","o","p","q","r","s","t",
+  "u","v","w","x","y","z"
+];
+
+var latest = "";
+
+function randomLetter() {
+  var char1 = chars[Math.floor(Math.random()*26)];
+  return ""+char1;
+}
+
+var letter = randomLetter();
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var a = [];
+var page;
+
+letter = randomLetter();
+
+// document.write("<h1>The Psychic Game</h1><br><br> Guess what letter I\'m thinking of<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a)
+
+document.onkeydown = checkKey;
+
+function checkKey(e) {
+    var event = window.event ? window.event : e;
+    a.push(event.key);
+    latest = event.key;
+    guesses = guesses--;
+	if (latest == letter) {
+		wins++;
+		guesses = 9;
+		letter = randomLetter();
+		a = [];
+		document.getElementById("body").innerHTML = ("<h1>The Psychic Game</h1><br><br> Guess what letter I\'m thinking of<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a)
+
+
+	}
+	else if (guesses < 1) {
+		losses++;
+		guesses = 9;
+		letter = randomLetter();
+		a = [];
+		document.getElementById("body").innerHTML = ("<h1>The Psychic Game</h1><br><br> Guess what letter I\'m thinking of<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a)
+
+	}
+	else {
+		guesses--;
+		document.getElementById("body").innerHTML = ("<h1>The Psychic Game</h1><br><br> Guess what letter I\'m thinking of<br><br>Wins: "+wins+"<br><br>Losses: "+losses+"<br><br>Guesses Left: "+guesses+"<br><br>Your Guesses so far: "+a)
+
+	}
+
+}
+
+
+	
